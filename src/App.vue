471f8c5fba669862999d83eef48fbf73bebdb905
邓宏明 <273900962@qq.com>
2017-05-02 10:31:14 +0800

    ---

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+      <hello></hello>
+  </div>
+</template>
+<script>
+import hello from './components/Hello'
+ export default{
+  name:'app',
+  components:{
+    'hello':hello
+  }
+ }
+</script>
+<style>
+</style>
